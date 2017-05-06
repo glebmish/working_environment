8512cd6cb8474593239bc7a409d6debb6d930be9
glebmish <gleb.mishchenko.96@gmail.com>
2017-05-06 20:01:10 +0300

    Better fonts installing

--- roles/dev_packages/scripts/install-source-code-pro-fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -f /usr/local/fonts/SourceCodePro-Medium.otf ]
-then
-  wget https://github.com/downloads/adobe-fonts/source-code-pro/SourceCodePro_FontsOnly-1.013.zip -O fonts.zip
-  mkdir /usr/share/source-code-pro
-  unzip fonts.zip -d /usr/share/fonts/source-code-pro
-  rm -f fonts.zip
-fi
-
